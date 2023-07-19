eae8c9c242a14bc233a6172dd4229e1e30d61570
Gleb Rysev <botkot1991@gmail.com>
2023-07-19 18:37:06 +0400

    done #14

--- #14.py
+++ #14.py
@@ -1,1 +1,15 @@
 #Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+n = abs(int(input('Введите число N ')))
+stop = 0
+p = 2
+for i in range(n):
+    if stop != 1:
+        p = p ** i
+        if p <= n:
+            print(p, end=' ')
+            p = 2
+        else:
+            stop = 1
+    else:
+        i = n
