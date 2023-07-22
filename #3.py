164ad22869a9663de55077fb852b6ee4ba19d7da
Gleb Rysev <botkot1991@gmail.com>
2023-07-22 22:15:54 +0400

    mod #3

--- #3.py
+++ #3.py
@@ -8,9 +8,20 @@
 #Output: 32
 
 
-class1=int(input("Введите кол-во учеников 1го класса: "))
-class2=int(input("Введите кол-во учеников 2го класса: "))
-class3=int(input("Введите кол-во учеников 3го класса: "))
+#class1=int(input("Введите кол-во учеников 1го класса: "))
+#class2=int(input("Введите кол-во учеников 2го класса: "))
+#class3=int(input("Введите кол-во учеников 3го класса: "))
 
-tables = (class1+1)//2 +(class2+1)//2 +(class3+1)//2
-print("Количество необходимых парт:",tables)
+#tables = (class1+1)//2 +(class2+1)//2 +(class3+1)//2
+## tables = (round((class1+1+class2+1+class3+1)//2))
+#print("Количество необходимых парт:",tables)
+
+firstCountStudy=int(input("Введите кол-во учеников 1го класса: "))
+secondCountStudy=int(input("Введите кол-во учеников 2го класса: "))
+thirdCountStudy=int(input("Введите кол-во учеников 3го класса: "))
+
+firstCountDesk= firstCountStudy//2+firstCountStudy%2
+secondCountDesk= secondCountStudy//2+secondCountStudy%2
+thirdCountDesk= thirdCountStudy//2+thirdCountStudy%2
+
+print(firstCountDesk+secondCountDesk+thirdCountDesk)
